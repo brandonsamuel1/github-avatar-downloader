62118ade49d12f49429bd62df66eef1017ca5108
Brandon Samuel <brandonsamuel72@gmail.com>
2017-11-07 02:07:29 +0000

    modified .gitignore and avatar program

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,22 @@
 var request = require('request');
 
+var secrets = require('secrets');
+
 console.log("Welcome to the GitHub Avatar Downloader!!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
 
+  headers {
+    'User-agent': 'request'
+    'Authorization': 'request'
+  }
+
+  };
+
+    request(url, function(err, res, body) {
+      cb(err, body);
+  })
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
