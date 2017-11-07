34ebc15968ebccd84e035c09f59c1271af096600
Brandon Samuel <brandonsamuel72@gmail.com>
2017-11-07 03:08:07 +0000

    updated .gitignore and avatar download

--- download_avatars.js
+++ download_avatars.js
@@ -1,25 +1,32 @@
-var request = require('request');
+const request = require('request');
 
-var secrets = require('secrets');
+require('dotenv').config();
+
+// const secrets = require('secrets');
 
 console.log("Welcome to the GitHub Avatar Downloader!!");
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-
-  headers {
-    'User-agent': 'request'
-    'Authorization': 'request'
-  }
-
+function getRepoContributors(repoOwner, repoName, callback) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    qs: {
+      access_token: process.env.GITHUB_API_TOKEN
+    },
+    headers: {
+      'User-agent': 'request'
+    }
   };
 
-    request(url, function(err, res, body) {
-      cb(err, body);
-  })
+  request(options, function(err, res, body) {
+    callback(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors: ", err)
-  console.log("Result: ", result)
+  // console.log("Result: ", result)
+  const contributors = JSON.parse(result);
+  for (var i = 0; i < contributors.length; i++) {
+    console.log(contributors[i].avatar_url);
+  }
 })
