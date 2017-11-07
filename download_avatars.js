fbf955d078bd477a29463ad971c88d61f4b981e1
Brandon Samuel <brandonsamuel72@gmail.com>
2017-11-07 04:10:16 +0000

    downloaded avatars for each user

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,8 @@
 const request = require('request');
-
+const fs = require('fs')
 require('dotenv').config();
+const owner = process.argv[2];
+const repo = process.argv[3];
 
 // const secrets = require('secrets');
 
@@ -22,11 +24,18 @@ function getRepoContributors(repoOwner, repoName, callback) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .pipe(fs.createWriteStream(filePath))
+  console.log(url);
+}
+
+
+getRepoContributors(owner, repo, function(err, result) {
   console.log("Errors: ", err)
   // console.log("Result: ", result)
   const contributors = JSON.parse(result);
   for (var i = 0; i < contributors.length; i++) {
-    console.log(contributors[i].avatar_url);
+    downloadImageByURL(contributors[i].avatar_url, `./downloads/${contributors[i].login}.jpg`);
   }
 })
